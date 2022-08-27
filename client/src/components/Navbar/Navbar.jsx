14af3581e1f55250a8813374593f44726dd1e70c
frost1335 <dilrozbek.developer@gmail.com>
2022-08-27 19:58:00 +0500

    fix: bug fixed in navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -31,82 +31,84 @@ const Navbar = () => {
     setNavMenu(newExpanded ? panel : false);
   };
 
-  return (
-    <div className="container">
-      <div className={`navbar__bg ${navMenu && "open"}`}>
-        <div className="navbar__accordion">
-          <Accordion
-            expanded={navMenu}
-            onChange={handleChange(true)}
-            className="accordion"
-          >
-            <AccordionSummary
-              aria-controls="panel1d-content"
-              id="accordion__button"
-            >
-              <div className="button__content">
-                <Link to="/" className="navbar__logo">
-                  <img src={logo} alt="navbar-logo" />
-                </Link>
-                <div className="item__lng">
-                  <button className="lng__button lng__button--active">
-                    UKR
-                  </button>
-                  <button className="lng__button">ENG</button>
-                </div>
-              </div>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </div>
-      </div>
-    </div>
-  );
-
   // return (
   //   <div className="container">
-  //     <div className="navbar">
-  //       <div className="navbar__logo">
-  //         <Link to="/">
-  //           <img src={logo} alt="navbar--logo" />
-  //         </Link>
-  //       </div>
-  //       <div className="navbar__menu">
-  //         <ul className="menu">
-  //           <li className="menu__item">
-  //             <Link to="/blog">Блог</Link>
-  //           </li>
-  //           <li className="menu__item">
-  //             <Link to="/courses">Курси</Link>
-  //           </li>
-  //           <li className="menu__item">
-  //             <Link to="/about">Про мене </Link>
-  //           </li>
-  //           <li className="menu__item">
-  //             <Link to="/" state={'subscribe'}>Підписка на сайт</Link>
-  //           </li>
-  //           <li className="menu__item">
-  //             <Link to="/support">Підтримка</Link>
-  //           </li>
-  //           <li className="menu__item menu__item--lng">
-  //             <div className="item__lng">
-  //               <button className="lng__button lng__button--active">UKR</button>
-  //               <button className="lng__button">ENG</button>
+  //     <div className={`navbar__bg ${navMenu && "open"}`}>
+  //       <div className="navbar__accordion">
+  //         <Accordion
+  //           expanded={navMenu}
+  //           onChange={handleChange(true)}
+  //           className="accordion"
+  //         >
+  //           <AccordionSummary
+  //             aria-controls="panel1d-content"
+  //             id="accordion__button"
+  //           >
+  //             <div className="button__content">
+  //               <Link to="/" className="navbar__logo">
+  //                 <img src={logo} alt="navbar-logo" />
+  //               </Link>
+  //               <div className="item__lng">
+  //                 <button className="lng__button lng__button--active">
+  //                   UKR
+  //                 </button>
+  //                 <button className="lng__button">ENG</button>
+  //               </div>
   //             </div>
-  //           </li>
-  //         </ul>
+  //           </AccordionSummary>
+  //           <AccordionDetails>
+  //             <Typography>
+  //               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  //               Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  //               eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  //               Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+  //               eget.
+  //             </Typography>
+  //           </AccordionDetails>
+  //         </Accordion>
   //       </div>
   //     </div>
   //   </div>
   // );
+
+  return (
+    <div className="container">
+      <div className="navbar">
+        <div className="navbar__logo">
+          <Link to="/">
+            <img src={logo} alt="navbar--logo" />
+          </Link>
+        </div>
+        <div className="navbar__menu">
+          <ul className="menu">
+            <li className="menu__item">
+              <Link to="/blog">Блог</Link>
+            </li>
+            <li className="menu__item">
+              <Link to="/courses">Курси</Link>
+            </li>
+            <li className="menu__item">
+              <Link to="/about">Про мене </Link>
+            </li>
+            <li className="menu__item">
+              <Link to="/" state={"subscribe"}>
+                Підписка на сайт
+              </Link>
+            </li>
+            <li className="menu__item">
+              <Link to="/support">Підтримка</Link>
+            </li>
+            <li className="menu__item menu__item--lng">
+              <div className="item__lng">
+                <button className="lng__button lng__button--active">UKR</button>
+                <button className="lng__button">ENG</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
