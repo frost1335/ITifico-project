19bc0ed09e19cf4127141f09d3fdf9e4ce4fb523
frost1335 <dilrozbek.developer@gmail.com>
2022-08-29 18:46:32 +0500

    add: about page translation added

--- client/src/features/About/components/Connect/Connect.jsx
+++ client/src/features/About/components/Connect/Connect.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 import { FaUser } from "react-icons/fa";
 import { BsFillEnvelopeFill } from "react-icons/bs";
+import { useTranslation } from "react-i18next";
 
 import "./Connect.scss";
 
 const Connect = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="about__connect">
       <div className="container">
         <div className="sendmail__content">
-          <h2 className="content__title">Зв'яжіться зі мною</h2>
+          <h2 className="content__title">{t("about_contact_title")}</h2>
           <form className="content__form">
             <div className="input__group">
-              <input type="text" placeholder="Повне ім’я" />
+              <input type="text" placeholder={t("about_input_name")} />
               <span className="input__icon">
                 <FaUser />
               </span>
             </div>
             <div className="input__group">
-              <input type="email" placeholder="Ваша Email адреса" />
+              <input type="email" placeholder={t("about_input_email")} />
               <span className="input__icon">
                 <BsFillEnvelopeFill />
               </span>
             </div>
-            <button>Зв’язатися</button>
+            <button>{t("about_button_text")}</button>
           </form>
         </div>
       </div>
--- client/src/features/About/components/Main/Main.jsx
+++ client/src/features/About/components/Main/Main.jsx
@@ -3,10 +3,13 @@ import { FaHome } from "react-icons/fa";
 import { HiArrowRight } from "react-icons/hi";
 import { Link } from "react-router-dom";
 import { AboutImg } from "../../assets";
+import { useTranslation } from "react-i18next";
 
 import "./Main.scss";
 
 const Main = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="about__main">
       <div className="container">
@@ -35,27 +38,14 @@ const Main = () => {
             <h1 className="banner__text">about</h1>
           </div>
           <div className="content__left">
-            <h1 className="main__title">Про мене</h1>
+            <h1 className="main__title">{t("about_title")}</h1>
+            <p className="left__text">{t("about_main_text1")}</p>
+            <p className="left__text">{t("about_main_text2")}</p>
             <p className="left__text">
-              Привіт! Я Стас. Я розробник повного стека більше 5 років. За час
-              свого розвитку я написав багато статей і записав відеопорад для
-              новачків і професіоналів. Я отримав багато позитивних відгуків,
-              але розумію, що цього недостатньо для початку.
-            </p>
-            <p className="left__text">
-              Багато молодих людей регулярно запитують мене, що я повинен
-              спочатку вчитися, щоб стати розробником.
-              <span className="left__text">
-                Ви хочете мати чітке та покрокове керівництво, які теми слід
-                вивчити в першу чергу. Тож цей сайт із курсами та статтями
-                розроблено для вас!
-              </span>
-            </p>
-            <p className="left__text">
-              Крім того, я надаю персональні консультації та навчання,{" "}
+              {t("about_main_text3")}
               <span className="text--outlined">
-                зв’яжіться зі мною, якщо ви хочете покращити свої навички
-                програмування:
+                {" "}
+                {t("about_main_text3_outlined")}
               </span>
             </p>
           </div>
