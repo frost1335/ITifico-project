891489e814b252eda019b435d2dbc9850b439899
frost1335 <dilrozbek.developer@gmail.com>
2022-10-01 19:54:22 +0500

    fix: fallback lang fixed

--- client/src/utils/i18next.js
+++ client/src/utils/i18next.js
@@ -1,9 +1,8 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
-i18next.use(initReactI18next).use(LanguageDetector).use(Backend).init({
+i18next.use(initReactI18next).use(Backend).init({
   saveMissing: true,
   fallbackLng: process.env.REACT_APP_FALLBACK_LANG,
 });
