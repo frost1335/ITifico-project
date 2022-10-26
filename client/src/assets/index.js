68b113d51e591492280339dac277270445075c3a
frost1335 <dilrozbek.developer@gmail.com>
2022-10-26 21:03:48 +0500

    fixed some bugs: deleted unnecessary imgs, fixed some files

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,5 +3,3 @@ export { default as pageBg2 } from "./pageBg2.svg";
 export { default as pageBg3 } from "./pageBg3.svg";
 export { default as pageBg4 } from "./pageBg4.svg";
 export { default as pageBg5 } from "./pageBg5.svg";
-export { default as defaultImg } from "./default-image.png";
-export { default as defaultIcon } from "./course-default.png";
