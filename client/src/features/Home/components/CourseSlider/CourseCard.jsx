22f89e395ec87164bfa46ab7531b00cbcffeb8a9
dilrozbek <dilrozbek.developer@gmail.com>
2022-12-18 12:56:38 +0500

    bug fixed at home page course card

--- client/src/features/Home/components/CourseSlider/CourseCard.jsx
+++ client/src/features/Home/components/CourseSlider/CourseCard.jsx
@@ -9,29 +9,29 @@ const CourseCard = ({ course }) => {
   const { t } = useTranslation();
 
   return (
-    <div className="course" style={{ background: course?.background }}>
-      <div className="course__banner">
-        <h2>new</h2>
+    <Link to={`/courses/view/${course._id}`} className="course__link--home">
+      <div className="course" style={{ background: course?.background }}>
+        <div className="course__banner">
+          <h2>new</h2>
+        </div>
+        <div className="course__icon">
+          <img
+            src={process.env.REACT_APP_BASE_URL + "/Uploads/" + course?.icon}
+            alt="course-icon"
+          />
+        </div>
+        <h4 className="course__title">{course?.[lng]?.title}</h4>
+        <p className="course__text">
+          {course?.[lng]?.description.length > homeSubCourseTextLaptop
+            ? course?.[lng]?.description.substring(0, homeSubCourseTextLaptop) +
+              "..."
+            : course?.[lng]?.description}
+        </p>
+        <div className="link__box">
+          <span className="box__link__link">{t("courses_detail_more")}</span>
+        </div>
       </div>
-      <div className="course__icon">
-        <img
-          src={process.env.REACT_APP_BASE_URL + "/Uploads/" + course?.icon}
-          alt="course-icon"
-        />
-      </div>
-      <h4 className="course__title">{course?.[lng]?.title}</h4>
-      <p className="course__text">
-        {course?.[lng]?.description.length > homeSubCourseTextLaptop
-          ? course?.[lng]?.description.substring(0, homeSubCourseTextLaptop) +
-            "..."
-          : course?.[lng]?.description}
-      </p>
-      <div className="link__box">
-        <Link className="box__link__link" to={`/courses/view/${course._id}`}>
-          {t("courses_detail_more")}
-        </Link>
-      </div>
-    </div>
+    </Link>
   );
 };
 
